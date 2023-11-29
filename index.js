3344508cd6094573643bc68de33407bdcaf4fff7
ismoilovsalohiddin <ismailovsalohiddin8@gmail.com>
2023-11-29 19:22:29 +0500

    finish

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const elLi = document.querySelectorAll(".order__right-selector")
+// const elLi = document.querySelectorAll(".order__right-selector")
+const elUl = document.querySelector(".order__right-lists")
 const elcards = document.querySelectorAll(".cards")
 const elButton = document.querySelectorAll(".order__right-button2")
-elLi.forEach(function(item, index){
-    item.addEventListener("click", function(){
-        elcards[index].classList.toggle("cards--open")
-        elButton[index].classList.toggle("button2--active")
-    }) 
+elUl.addEventListener("click", function(event){
+    const elLi = event.target.closest(".order__right-item")
+    if(elLi){
+        elLi.querySelector(".cards").classList.toggle("cards--open")
+        elLi.querySelector(".order__right-button2").classList.toggle("button2--active")
+
+    }
+
 })
+// elLi.forEach(function(item, index)
+//     item.addEventListener("click", function(){
+//         elcards[index].classList.toggle("cards--open")
+//         elButton[index].classList.toggle("button2--active")
+//     }) 
+// })
 
-// vazifa 2
-
-// inputlar checked bolsa boxni ichidagi spanlar inputlarni valuesiga ozgarishi kerak
 
 const elForm = document.querySelector(".order__form")
 
@@ -22,23 +29,23 @@ const elOrder5 = document.querySelector(".js--order5")
 
 const elInputs = document.querySelectorAll(".input1")
 
-elInputs.forEach(function(item){
-    elForm.addEventListener("click", function(event){
-        event.preventDefault
-        if(item.checked && item.name === "input1"){
-            elOrder1.textContent = item.value
-        }
-        if(item.checked && item.name === "input2"){
-            elOrder2.textContent = item.value
-        }
-        if(item.checked && item.name === "input3"){
-            elOrder3.textContent = item.value
-        }
-        if(item.checked && item.name === "input4"){
-            elOrder4.textContent = item.value
-        }
-        if(item.checked && item.name === "input5"){
-            elOrder5.textContent = item.value
-        }
-    })
+
+
+elForm.addEventListener("change", function(event){
+    event.preventDefault
+    if(event.target.name === "input1"){
+        elOrder1.textContent = event.target.value
+    }
+    if(event.target.name === "input2"){
+        elOrder2.textContent = event.target.value
+    }
+    if(event.target.name === "input3"){
+        elOrder3.textContent = event.target.value
+    }
+    if(event.target.name === "input4"){
+        elOrder4.textContent = event.target.value
+    }
+    if(event.target.name === "input5"){
+        elOrder5.textContent = event.target.value
+    }
 })
